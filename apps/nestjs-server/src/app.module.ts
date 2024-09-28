1f66593fc9a80672689b5cc6de4a329cac992922
Flab-Gary <166278213+Flab-Gary@users.noreply.github.com>
2024-09-28 23:15:54 +0900

    Develop (#4)
    * chore: remove nest js code and react code (#3)
    
    Co-authored-by: Cho-Geonwoo <joungju257@gmail.com>
    
    * [#1] 프로젝트 시작 전 기본 셋팅 (#2)
    
    * chore: ESLint 설정에 Prettier 플러그인과 LF 규칙 추가
    
    * chore: ESLint 줄바꿈 스타일을 유닉스(LF)로 설정
    
    * chore: 프로젝트 초기 셋팅
    
    * chore: ESLint 및 Prettier 설정 수정
    
    * reractor: 타입스크립트 인터페이스 타입 추가
    
    * refactor: zustand api 호출 테스트
    
    ---------
    
    Co-authored-by: Cho-Geonwoo <joungju257@gmail.com>
    Co-authored-by: kimhyein <70068498+henny1105@users.noreply.github.com>

--- apps/nestjs-server/src/app.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { HomeModule } from './routes';
-
-@Module({
-  imports: [HomeModule],
-  controllers: [],
-  providers: []
-})
-export class AppModule {}
---
+++ apps/next-client/src/app/pharmacy/page.tsx
@@ -0,0 +1,7 @@
+export default function PharmacyPage() {
+  return (
+    <div>
+      <h1>약국 찾기</h1>
+    </div>
+  );
+}
