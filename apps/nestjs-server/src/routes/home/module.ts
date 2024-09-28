948f3bac7d0e70071a3aa82033969561b243a790
Flab-Gary <166278213+Flab-Gary@users.noreply.github.com>
2024-09-28 23:14:53 +0900

    chore: remove nest js code and react code (#3)
    Co-authored-by: Cho-Geonwoo <joungju257@gmail.com>

--- apps/nestjs-server/src/routes/home/module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { HomeController } from './controller';
-import { HomeService } from './service';
-
-@Module({
-  controllers: [HomeController],
-  providers: [HomeService]
-})
-export class HomeModule {}
