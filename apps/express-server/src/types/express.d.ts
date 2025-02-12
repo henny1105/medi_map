94729b6b4d5c6654fb2cb39faa89ab1a651ddf6a
kimhyein <70068498+henny1105@users.noreply.github.com>
2025-02-12 19:01:52 +0900

    [#115] 워크플로우 오류 관련 해결 (#116)
    * chore: workflow 수정 및 버전 충돌 문제 해결
    
    * chore: 모노레포 tsconfig 삭제
    
    * chore 빌드 오류 해결

--- apps/express-server/src/types/express.d.ts
+++ apps/express-server/src/types/express.d.ts
@@ -8,8 +8,6 @@ declare global {
   namespace Express {
     interface Request {
       user?: CustomJwtPayload & { username?: string };
-      file?: Multer.File;
-      files?: { [fieldname: string]: Multer.File[] } | Multer.File[];
     }
   }
 }
